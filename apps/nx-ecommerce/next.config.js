1dc3e282d9ed362e7b10b1fc7032378bf485ee96
DevWaterdrop <dev.waterdrop@gmail.com>
2022-06-07 14:05:47 +0200

    Install "windicss"

--- apps/nx-ecommerce/next.config.js
+++ apps/nx-ecommerce/next.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -9,6 +10,10 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin());
+    return config;
   },
 };
 
