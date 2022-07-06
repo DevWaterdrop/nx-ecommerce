6894600e4107ff66248ebc08735a2f699e04429b
DevWaterdrop <dev.waterdrop@gmail.com>
2022-07-06 14:11:32 +0200

    Fix Prettier && Run format
    - Ignore for Prettier: Strapi cache, etc.
    - Fix CI

--- apps/strapi/src/api/fast-user/controllers/fast-user.ts
+++ apps/strapi/src/api/fast-user/controllers/fast-user.ts
@@ -2,6 +2,6 @@
  *  fast-user controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::fast-user.fast-user');
--- apps/strapi/src/api/home-page/controllers/home-page.ts
+++ apps/strapi/src/api/home-page/controllers/home-page.ts
@@ -2,6 +2,6 @@
  *  home-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::home-page.home-page');
