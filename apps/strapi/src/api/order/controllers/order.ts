4361dbea65a90f7adbc4e40fe772601856fcb2f4
DevWaterdrop <dev.waterdrop@gmail.com>
2022-08-07 21:26:29 +0200

    Format with Prettier

--- apps/strapi/src/api/order/controllers/order.ts
+++ apps/strapi/src/api/order/controllers/order.ts
@@ -2,6 +2,6 @@
  *  order controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::order.order');
