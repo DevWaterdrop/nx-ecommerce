f282ea0efc6cffc00563157118a10c54b9225ad5
DevWaterdrop <dev.waterdrop@gmail.com>
2022-08-09 21:43:24 +0200

    Refactor className structure

--- apps/nx-ecommerce/pages/404.tsx
+++ apps/nx-ecommerce/pages/404.tsx
@@ -1,6 +1,10 @@
+import clsx from 'clsx';
+
 const FourOhFour: React.FC = () => {
   return (
-    <section className="flex flex-col m-auto w-max py-12 gap-6 items-center">
+    <section
+      className={clsx('m-auto w-max py-12', 'flex flex-col gap-6 items-center')}
+    >
       <h1 className="font-semibold text-3xl">404 - Page Not Found</h1>
       <small>Keep fighting</small>
     </section>
