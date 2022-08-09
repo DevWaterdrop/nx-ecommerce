66c2fb7f607209a2f88fb393ec0f164d62b84369
DevWaterdrop <dev.waterdrop@gmail.com>
2022-08-10 00:13:25 +0200

    Fix `getBaseURL`

--- apps/nx-ecommerce/utils/get-base-url.ts
+++ apps/nx-ecommerce/utils/get-base-url.ts
@@ -1,5 +1,5 @@
 export function getBaseURL(path = '') {
   return `${
-    (process.env['NX_BASE_URL'] as string) || 'http://localhost:1337'
+    (process.env['NX_BASE_URL'] as string) || window.location.origin
   }${path}`;
 }
