36a82951e7840e156a91dd0a2b167bb0903708f7
DevWaterdrop <dev.waterdrop@gmail.com>
2022-08-09 21:40:49 +0200

    Add client/server error handlers/scenes
    Closes #28

--- apps/nx-ecommerce/pages/_app.tsx
+++ apps/nx-ecommerce/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ErrorBoundary } from '../components/ErrorBoundary';
 import 'windi.css';
 import './styles.css';
 
@@ -8,10 +9,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    // Don`t want to use Hydrate because of more boilerplate
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ErrorBoundary>
+      {/* Don`t want to use Hydrate because of more boilerplate */}
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
